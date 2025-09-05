177a09d200aa2c90d195152ef4d70e5a25981b7b
Carla <carla.magalhaes404@gmail.com>
2025-09-05 18:23:48 -0300

    server-http

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Server is running...");
+// implementação do http
+import * as http from "http";
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {});
+
+const port  =   process.env.PORT
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+// final da implementação do http
