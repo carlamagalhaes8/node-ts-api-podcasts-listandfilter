7ee5d94894303b981983b3dc2e539578aa7fe805
Carla <carla.magalhaes404@gmail.com>
2025-09-06 10:30:16 -0300

    refatorando-rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // implementação do http
 import * as http from "http";
 import {getFilterEpisodes, getListEpisodes} from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
 
 const server = http.createServer(
     async (request: http.IncomingMessage, response: http.ServerResponse) => {
@@ -9,11 +10,11 @@ const server = http.createServer(
         const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
         
         // listar podcasts
-        if (request.method === "GET" && baseUrl === "/api/list") {
+        if (request.method === "GET" && baseUrl === Routes.LIST) {
             await getListEpisodes(request, response);
         }
 
-        if (request.method === "GET" && baseUrl === "/api/episode") {
+        if (request.method === "GET" && baseUrl === Routes.EPISODE) {
             await getFilterEpisodes(request, response);
         }
     });
