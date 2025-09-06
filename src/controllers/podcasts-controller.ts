f8463e71695718d5eb2fbd957c9df7930f329511
Carla <carla.magalhaes404@gmail.com>
2025-09-06 12:30:25 -0300

    project created

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -4,13 +4,15 @@ import { serviceFilterEpisodes } from "../services/filter-episodes";
 import { contentType } from "../utils/content-type";
 import { podcastTransferModel } from "../models/podcast-transfer-model";
 
+const defaultContent = {'Content-Type': contentType.JSON};
+
 export const getListEpisodes = async (
     request: IncomingMessage, 
     response: ServerResponse) => {
 
         const content: podcastTransferModel = await serviceListEpisodes();
 
-    response.writeHead(content.statusCode, {'Content-Type': contentType.JSON});
+    response.writeHead(content.statusCode, defaultContent);
     response.write(JSON.stringify(content.body));
 
     response.end();
@@ -22,7 +24,7 @@ export const getFilterEpisodes = async (
 
         const content: podcastTransferModel = await serviceFilterEpisodes(request.url);
 
-        response.writeHead(content.statusCode, {'Content-Type': contentType.JSON});
+        response.writeHead(content.statusCode, defaultContent);
         response.write(JSON.stringify(content.body));
         response.end();
     };
